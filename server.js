4b97b3d2082a07dd3e7b8a79c4c7270d06a23798
D.Ghazvinie <68420629+Ghazvinie@users.noreply.github.com>
2021-04-15 19:30:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ class Server extends EventEmitter {
         process.nextTick(() => {
             this.emit('response', 'Type a command, or \'help\' to list all commands');
         });
+        
         client.on('command', (command, args) => {
             switch (command) {
                 case 'help':
@@ -25,15 +26,18 @@ class Server extends EventEmitter {
             }
         });
     }
+    
     tasksString() {
         return Object.keys(this.tasks).map(element => `\n Task ${element}: ${this.tasks[element]}`).join(' ');
     }
+    
     saveTasks() {
         const data = JSON.stringify(this.tasks);
         fs.writeFile('tasks.json', data, 'utf8', (error) => {
             if (error) console.log('There as been a save error', error);
         });
     }
+    
     async loadTasks() {
         return new Promise((resolve, reject) => {
             fs.readFile('tasks.json', 'utf8', (error, data) => {
@@ -46,6 +50,7 @@ class Server extends EventEmitter {
             });
         });
     }
+    
     async deleteLocalFile () {
         return new Promise ((resolve, reject) => {
             fs.unlink('tasks.json', (error) => {
@@ -67,12 +72,14 @@ class Server extends EventEmitter {
         load (tasks from local file - will overwrite any currently stored tasks)
         deleteLocal (deletes local tasks file - this cannot be undone!)`);
     }
+    
     add(args) {
         this.tasks[this.taskId] = args.join(' ');
         console.log(this.tasks);
         this.emit('response', `Added the task(s) ${this.tasks[this.taskId]}\nTask Number: ${this.taskId} `);
         this.taskId++;
     }
+    
     ls() {
         if (Object.keys(this.tasks).length === 0) {
             this.emit('response', 'There are no tasks, try adding one');
@@ -80,10 +87,12 @@ class Server extends EventEmitter {
             this.emit('response', `The tasks are: ${this.tasksString()} `);
         }
     }
+    
     delete(args) {
         delete (this.tasks[args[0]]);
         this.emit('response', `Deleted task ${args[0]}`);
     }
+    
     save() {
         if (Object.keys(this.tasks).length === 0) {
             this.emit('response', 'No tasks to be saved');
@@ -92,10 +101,12 @@ class Server extends EventEmitter {
             this.emit('response', `${Object.keys(this.tasks).length} task(s) have been saved`);
         }
     }
+    
     async load() {
         await this.loadTasks();
         this.emit('response', `${Object.keys(this.tasks).length} task(s) have been loaded`);
     }
+    
     async deleteLocal() {
         await this.deleteLocalFile();
         this.emit('response', 'Local tasks file has been deleted');        
