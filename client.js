f6123b0bbdddafc87499ad9448c34ba8acf2e91b
D.Ghazvinie <68420629+Ghazvinie@users.noreply.github.com>
2021-04-15 19:29:57 +0100

    Update client.js

--- client.js
+++ client.js
@@ -7,9 +7,7 @@ const readLine = readline.createInterface({
 });
 
 const client = new EventEmitter();
-
 const server  = require('./server')(client);
-
 
 server.on('response', (response) => {
     process.stdout.write('\u001B[2J\u001B[0;0f');
@@ -20,9 +18,7 @@ server.on('response', (response) => {
 
 let command, args;
 
-
 readLine.on('line', (input) => {
     [command, ...args] = input.split(' ');
     client.emit('command', command, args);
 });
-
